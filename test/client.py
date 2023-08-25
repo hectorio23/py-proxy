b8dc1ffaeb16abd1c67533c746ff8c0d1f1f6e04
hectorio23 <hectorino2789@gmail.com>
2023-08-24 20:55:50 -0600

    0.03 Other type of server -> proxy_handler_class.py

--- test/client.py
+++ test/client.py
@@ -1,69 +1,21 @@
-# import asyncio
-
-
-# async def ProxyHandler():
-#     reader, writer = await asyncio.open_connection(
-#         '127.0.0.1', 
-#         9999,
-#         limit=1024,
-#         ssl=None,
-#         family=0,
-#         proto=0,
-#         flags=0,
-#         sock=None,
-#         local_addr=None,
-#         # server_hostname="https://adan.com",
-#         # ssl_handshake_timeout=0,
-#         # ssl_shutdown_timeout=0,
-#         happy_eyeballs_delay=0,
-#         interleave=0
-#         )
-    
-#     writer.write("Esta es una pinche prueba sobre esto".encode('UTF-8'))
-#     await writer.drain()
-
-#     data:str = await reader.read(-1)
-#     print(reader.at_eof())
-#     print(data.decode('UTF-8'))
-
-#     writer.close()
-#     await writer.wait_closed()
-
-
-# asyncio.run(ProxyHandler())
-
+# /usr/bin/python3
 import asyncio
-import urllib.parse
-import sys
 
-async def print_http_headers(url):
-    url = urllib.parse.urlsplit(url)
-    if url.scheme == 'https':
-        reader, writer = await asyncio.open_connection(
-            url.hostname, 443, ssl=True)
-    else:
-        reader, writer = await asyncio.open_connection(
-            url.hostname, 80)
+async def main():
+    config = {
+        "host": "127.0.0.1",
+        "port": 8080
+    }
 
-    query = (
-        f"HEAD {url.path or '/'} HTTP/1.0\r\n"
-        f"Host: {url.hostname}\r\n"
-        f"\r\n"
-    )
+    reader, writer = await asyncio.open_connection(**config)
 
-    writer.write(query.encode('latin-1'))
-    while True:
-        line = await reader.readline()
-        if not line:
-            break
+    writer.write(b"https://github.com/")
+    await writer.drain()
 
-        line = line.decode('latin1').rstrip()
-        if line:
-            print(f'HTTP header> {line}')
+    data = await reader.read(-1)
+    print(f"El mensage recibido por el server es: {data.decode('UTF-8')}")
 
-    # Ignore the body, close the socket
     writer.close()
     await writer.wait_closed()
 
-url = sys.argv[1]
-asyncio.run(print_http_headers(url))
+asyncio.run(main())
---
+++ test/proxy_handler_class.py
@@ -0,0 +1,51 @@
+# /usr/bin/python3
+'''
+hectorio23
+
+Cabe recalcar que esto esta en fase de desarrollo, Se necesita
+aun aplicar suficiente testeo para comprobar que no haya `bugs`
+o comportamiento no programado.
+Este servidor basico no puede realizar peticiones HTTPS, aun no 
+esta implementado.
+'''
+
+import socketserver
+import http.server
+import http.client
+
+class ProxyHandler(http.server.BaseHTTPRequestHandler):
+    '''This Class creates and heandle the Navigator request
+
+        As su can read, this class is the worker who `intercept` the navigator
+        request and send the request for it and send again the response to the 
+        navigator.
+        It's important to get a good privacity now for the internet, for that, more later 
+        I will implement some functinalities who allowed to us to change the request headers.
+
+    '''
+    def do_GET(self):
+        '''Tries to send the response'''
+        try:
+            # Conectarse al servidor remoto
+            remote = http.client.HTTPConnection(self.path)
+            remote.request("POST", self.path)
+            response = remote.getresponse()
+
+            # Enviar la respuesta al cliente (navegador)
+            self.send_response(response.status)
+            for header, value in response.getheaders():
+                self.send_header(header, value)
+            self.end_headers()
+            self.wfile.write(response.read())
+            print("Peticion enviada")
+        except Exception as e:
+            '''Send the error if there is'''
+            self.send_error(500, f"Error: {e}")
+
+def run_proxy_server():
+    '''Creates the server and listen in the following port'''
+    port = 9090  
+    with socketserver.ThreadingTCPServer(("127.0.0.1", port), ProxyHandler) as server:
+        print(f"Proxy en funcionamiento en el puerto {port}")
+        server.serve_forever()
+
