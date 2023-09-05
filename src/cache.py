cb0521b8f662d930094ecf736ae9c25be00a82c4
hectorio23 <hectorino2789@gmail.com>
2023-09-04 19:09:10 -0600

    0.05 Working on Cache-ProxyServer file and built it

---
+++ src/cache.py
@@ -0,0 +1,9 @@
+# /usr/bin/python3
+
+# This module provides a Cache Server class 
+# that allow to the server keep saved the Web pages
+# with the only objetive to be faster at the time
+# to make a request
+
+class CacheServer:
+    pass
