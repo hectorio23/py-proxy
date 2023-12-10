eb09de248cd95558df4728492a28f5220b20a603
hectorio23 <hectorino2789@gmail.com>
2023-12-09 22:30:07 -0600

    0.05 Fixing some methods

--- test/proxy_handler_class.py
+++ test/proxy_handler_class.py
@@ -1,67 +1,64 @@
-# /usr/bin/python3
-'''
-hectorio23
-
-Cabe recalcar que esto esta en fase de desarrollo, Se necesita
-aun aplicar suficiente testeo para comprobar que no haya `bugs`
-o comportamiento no programado.
-Este servidor basico no puede realizar peticiones HTTPS, aun no 
-esta implementado.
-'''
-
 import socketserver
 import http.server
 import http.client
+import urllib.parse
 
 class ProxyHandler(http.server.BaseHTTPRequestHandler):
-    '''This Class creates and heandle the Navigator request
-
-        As su can read, this class is the worker who `intercept` the navigator
-        request and send the request for it and send again the response to the 
-        navigator.
-        It's important to get a good privacity now for the internet, for that, more later 
-        I will implement some functinalities who allowed to us to change the request headers.
-
-    '''
     def do_GET(self):
-        '''Tries to send the response'''
-        try:
-            # Conectarse al servidor remoto
-            remote = http.client.HTTPConnection(self.path)
-            remote.request("GET", self.path)
-            response = remote.getresponse()
-
-            # Enviar la respuesta al cliente (navegador)
-            self.send_response(response.status)
-            for header, value in response.getheaders():
-                self.send_header(header, value)
-            self.end_headers()
-            self.wfile.write(response.read())
-            print("Peticion enviada")
-        except Exception as e:
-            '''Send the error if there is'''
-            self.send_error(500, f"Error: {e}")
-    
-    def do_CONNECT(self):
-        print(self.path)
-        remote = http.client.HTTPConnection(self.path)
-        remote.request("GET", self.path)
+        url = urllib.parse.urlsplit(self.path)
+        remote = http.client.HTTPSConnection(host=url.netloc, port=443, timeout=10)
+        remote.request("GET", url=url.path)
         response = remote.getresponse()
 
         self.send_response(response.status)
         for header, value in response.getheaders():
             self.send_header(header, value)
-        self.send_response(response.code)
         self.end_headers()
-        self.wfile.write(b"Hola bro XD")
 
+        self.wfile.write(response.read())
+
+    def do_POST(self):
+        url = urllib.parse.urlsplit(self.path)
+        remote = http.client.HTTPSConnection(host=url.netloc, port=443, timeout=5)
+        remote.request("POST", url=url.path)
+        response = remote.getresponse()
+        print(self.path)
+
+        self.send_response(response.status)
+        for header, value in response.getheaders():
+            self.send_header(header, value)
+        self.end_headers()
+
+        self.wfile.write(response.read())
+
+    def do_CONNECT(self):
+        # Obtener el host y el puerto del CONNECT request
+        host, port = self.path.split(':')
+        port = int(port)
+
+        # Establecer la conexión con el servidor remoto
+        remote = http.client.HTTPSConnection(host, port, timeout=5)
+        remote.connect()
+
+        # Enviar la respuesta al cliente (navegador)
+        self.send_response(200, 'Connection established')
+        self.end_headers()
+
+        # Configurar el reenvío de datos entre el cliente y el servidor
+        self.connection.sendall(b'HTTP/1.1 200 Connection established\r\n\r\n')
+        self.connection = remote.sock
+
+        # Leer datos del cliente y reenviar al servidor
+        while True:
+            data = self.connection.recv(4096)
+            if not data:
+                break
+            self.connection.sendall(data)
 
 def run_proxy_server():
-    '''Creates the server and listen in the following port'''
-    port = 9090  
+    port = 9090
     with socketserver.ThreadingTCPServer(("127.0.0.1", port), ProxyHandler) as server:
         print(f"Proxy en funcionamiento en el puerto {port}")
         server.serve_forever()
-
 
 run_proxy_server()
